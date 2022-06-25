6279e8edca8c230dcd764a578c201cb981c6ec9a
Lars Thorup <lars@zealake.com>
2022-06-25 19:31:43 +0200

    withServer

--- src/withFixture.test.ts
+++ src/withFixture.test.ts
@@ -26,27 +26,27 @@ const test: TestWith<{}> = {
 const use = <T extends {}, U extends {}>(
   test: TestWith<T>,
   get: () => U,
-  block: BlockWith<U>
+  block: BlockWith<T & U>
 ) => {
   block({
     ...test,
     ...get(),
-    afterAll: (fn: BlockWith<U>) => {
+    afterAll: (fn: BlockWith<T & U>) => {
       test.afterAll((test: TestWith<T>) => {
         fn({ ...test, ...get() } as TestWith<T & U>);
       });
     },
-    beforeAll: (fn: BlockWith<U>) => {
+    beforeAll: (fn: BlockWith<T & U>) => {
       test.beforeAll((test: TestWith<T>) => {
         fn({ ...test, ...get() } as TestWith<T & U>);
       });
     },
-    describe: (name: string, fn: BlockWith<U>) => {
+    describe: (name: string, fn: BlockWith<T & U>) => {
       test.describe(name, (test: TestWith<T>) => {
         fn({ ...test, ...get() } as TestWith<T & U>);
       });
     },
-    it: (name: string, fn: BlockWith<U>) => {
+    it: (name: string, fn: BlockWith<T & U>) => {
       test.it(name, (test: TestWith<T>) => {
         fn({ ...test, ...get() } as TestWith<T & U>);
       });
@@ -62,7 +62,6 @@ test.describe("withFixture, typed", () => {
     const withDb = (test: TestWith<{}>, block: BlockWith<{ db: Db }>) => {
       test.describe("useDb", () => {
         let db: Db;
-        const get = () => ({ db });
         test.beforeAll(() => {
           db = { some: "db" };
           order.push("setup db");
@@ -71,13 +70,16 @@ test.describe("withFixture, typed", () => {
           db = undefined;
           order.push("teardown db");
         });
-        use(test, get, block);
+        use(test, () => ({ db }), block);
       });
     };
-    withDb(test, (test) => {
-      test.describe("withServer", () => {
-        type Server = { db: Db };
-        let server: Server | undefined;
+    type Server = { db: Db } | undefined;
+    const withServer = (
+      test: TestWith<{ db: Db }>,
+      block: BlockWith<{ db: Db; server: Server }>
+    ) => {
+      test.describe("withServer", ({ db }) => {
+        let server: Server;
         test.beforeAll(({ db }) => {
           server = { db };
           order.push("setup server");
@@ -86,7 +88,12 @@ test.describe("withFixture, typed", () => {
           server = undefined;
           order.push("teardown server");
         });
-        test.it("should have setup", ({ db }) => {
+        use(test, () => ({ server }), block);
+      });
+    };
+    withDb(test, (test) => {
+      withServer(test, (test) => {
+        test.it("should have setup", ({ db, server }) => {
           expect(server).toEqual({ db: { some: "db" } });
           expect(db).toEqual({ some: "db" });
           order.push("test");
